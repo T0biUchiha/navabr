3156a63bad3c245f97347d0566009843159c42b8
Aditya Sharma <101574252+T0biUchiha@users.noreply.github.com>
2022-05-16 19:51:08 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+// import {Card} from '../src/components/Card';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+        <div id='logo'>LOGOBAKERY</div>
+        <div className='text'><p>Services</p> <p>Projects</p> <p>About</p> </div>
+        <div className="btn"><button id='btn'>Contact</button></div>
+        
+
+    </div>
+  );
+}
+
+export default App;
